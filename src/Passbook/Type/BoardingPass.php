7f68849a238dc46277a1dc03862a1edb7755591b
Eymen Gunay <eymen@egunay.com>
2013-06-15 15:44:14 +0200

    Transit types are added as constants to boarding pass

--- src/Passbook/Type/BoardingPass.php
+++ src/Passbook/Type/BoardingPass.php
@@ -38,16 +38,48 @@ class BoardingPass extends Pass
 
     /**
      * Type of transit. Must be one of the following values:
-     * PKTransitTypeAir, PKTransitTypeBoat, PKTransitTypeBus, PKTransitTypeGeneric,PKTransitTypeTrain
+     * PKTransitTypeAir, PKTransitTypeBoat, PKTransitTypeBus, PKTransitTypeGeneric, PKTransitTypeTrain
      * @var string
      */
     protected $transitType;
 
-    public function __construct($serialNumber, $name, $description, $transitType)
+    /**
+     * @var string
+     */
+    const TYPE_AIR = 'PKTransitTypeAir';
+
+    /**
+     * @var string
+     */
+    const TYPE_BOAT = 'PKTransitTypeBoat';
+
+    /**
+     * @var string
+     */
+    const TYPE_BUS = 'PKTransitTypeBus';
+
+    /**
+     * @var string
+     */
+    const TYPE_GENERIC = 'PKTransitTypeGeneric';
+
+    /**
+     * @var string
+     */
+    const TYPE_TRAIN = 'PKTransitTypeTrain';
+
+    /**
+     * Class constructor
+     *
+     * @param string $serialNumber
+     * @param string $description
+     * @param string $transitType
+     */
+    public function __construct($serialNumber, $description, $transitType = self::TYPE_GENERIC)
     {
         // Required for boarding passes; otherwise not allowed
         $this->$transitType = $transitType;
         // Call parent
-        parent::__construct($serialNumber, $name, $description);
+        parent::__construct($serialNumber, $description);
     }
 }
