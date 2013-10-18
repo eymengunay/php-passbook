318aeeae6e7651632be946e5544765e546cfe4fe
Eymen Gunay <eymen@egunay.com>
2013-10-18 15:37:50 +0200

    More tests added for pass classes

--- tests/Passbook/Tests/PassbookTest.php
+++ tests/Passbook/Tests/PassbookTest.php
@@ -2,15 +2,17 @@
 
 namespace Passbook\Tests;
 
-use Passbook\Pass\Field;
 use Passbook\PassFactory;
+use Passbook\Pass\Field;
 use Passbook\Pass\Barcode;
+use Passbook\Pass\Location;
 use Passbook\Pass\Structure;
 use Passbook\Type\BoardingPass;
 use Passbook\Type\Coupon;
 use Passbook\Type\EventTicket;
 use Passbook\Type\Generic;
 use Passbook\Type\StoreCard;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 class PassbookTest extends \PHPUnit_Framework_TestCase
 {
@@ -49,6 +51,10 @@ class PassbookTest extends \PHPUnit_Framework_TestCase
         $this->eventTicket->setLogoText('Apple Inc.');
         $this->assertSame('Apple Inc.', $this->eventTicket->getLogoText());
 
+        // Add location
+        $location = new Location(59.33792, 18.06873);
+        $this->eventTicket->addLocation($location);
+
         // Create pass structure
         $structure = new Structure();
 
@@ -72,13 +78,60 @@ class PassbookTest extends \PHPUnit_Framework_TestCase
 
         $barcode = new Barcode('PKBarcodeFormatQR', 'barcodeMessage');
         $this->eventTicket->setBarcode($barcode);
+
+        $json = PassFactory::serialize($this->eventTicket);
+        $array = json_decode($json, true);
+
+        $this->assertArrayHasKey('locations', $array);
+        $this->assertArrayHasKey('barcode', $array);
+        $this->assertArrayHasKey('logoText', $array);
+        $this->assertArrayHasKey('backgroundColor', $array);
+        $this->assertArrayHasKey('eventTicket', $array);
     }
-    
+
+    /**
+     * Generic
+     */
+    public function testGeneric()
+    {
+        $this->generic->setBackgroundColor('rgb(60, 65, 76)');
+        $this->assertSame('rgb(60, 65, 76)', $this->generic->getBackgroundColor());
+        $this->generic->setLogoText('Apple Inc.');
+        $this->assertSame('Apple Inc.', $this->generic->getLogoText());
+
+        // Create pass structure
+        $structure = new Structure();
+
+        // Add primary field
+        $primary = new Field('event', 'The Beat Goes On');
+        $primary->setLabel('Event');
+        $structure->addPrimaryField($primary);
+
+        // Add back field
+        $back = new Field('back', 'Hello World!');
+        $back->setLabel('Location');
+        $structure->addSecondaryField($back);
+
+        // Add auxiliary field
+        $auxiliary = new Field('datetime', '2013-04-15 @10:25');
+        $auxiliary->setLabel('Date & Time');
+        $structure->addAuxiliaryField($auxiliary);
+
+        // Set pass structure
+        $this->generic->setStructure($structure);
+
+        $barcode = new Barcode('PKBarcodeFormatQR', 'barcodeMessage');
+        $this->generic->setBarcode($barcode);
+    }
+
     /**
      * {@inheritdoc}
      */
     protected function setUp()
     {
+        $loader = require_once __DIR__ . "/../../../vendor/autoload.php";
+        AnnotationRegistry::registerAutoloadNamespace('JMS\Serializer\Annotation', __DIR__ . "/../../../vendor/jms/serializer/src");
+
         $this->boardingPass = new BoardingPass(uniqid(), 'Lorem ipsum', 'PKTransitTypeBoat');
         $this->coupon       = new Coupon(uniqid(), 'Lorem ipsum');
         $this->eventTicket  = new EventTicket(uniqid(), 'Lorem ipsum');
