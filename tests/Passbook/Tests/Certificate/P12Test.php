9b24c49a8c889177bef72111086583c42839bf64
Eymen Gunay <eymen@egunay.com>
2014-02-20 01:41:47 +0100

    Certificate exception tests added

--- tests/Passbook/Tests/Certificate/P12Test.php
+++ tests/Passbook/Tests/Certificate/P12Test.php
@@ -3,6 +3,7 @@
 namespace Passbook\Tests\Pass;
 
 use Passbook\Certificate\P12;
+use Passbook\Exception\FileNotFoundException;
 
 class P12Test extends \PHPUnit_Framework_TestCase
 {
@@ -11,5 +12,12 @@ class P12Test extends \PHPUnit_Framework_TestCase
         $p12 = new P12(__DIR__.'/../../../cert/dummy.p12', '123456');
 
         $this->assertEquals($p12->getPassword(), '123456');
+    }
+
+    public function testP12Exception()
+    {
+        $this->setExpectedException('Passbook\Exception\FileNotFoundException');
+
+        $wwdr = new P12(__DIR__.'/non-existing-file', '123456');
     }
 }
--- tests/Passbook/Tests/Certificate/WWDRTest.php
+++ tests/Passbook/Tests/Certificate/WWDRTest.php
@@ -3,11 +3,19 @@
 namespace Passbook\Tests\Pass;
 
 use Passbook\Certificate\WWDR;
+use Passbook\Exception\FileNotFoundException;
 
 class WWDRTest extends \PHPUnit_Framework_TestCase
 {
     public function testWWDR()
     {
         $wwdr = new WWDR(__DIR__.'/../../../cert/dummy.wwdr');
+    }
+
+    public function testWWDRException()
+    {
+        $this->setExpectedException('Passbook\Exception\FileNotFoundException');
+
+        $wwdr = new WWDR(__DIR__.'/non-existing-file');
     }
 }
