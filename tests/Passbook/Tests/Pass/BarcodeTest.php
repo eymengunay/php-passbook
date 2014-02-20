44d08fee3dfcdb2e892985c9d1e759daedc5b6b8
Eymen Gunay <eymen@egunay.com>
2014-02-20 02:02:42 +0100

    Barcode test updated

--- tests/Passbook/Tests/Pass/BarcodeTest.php
+++ tests/Passbook/Tests/Pass/BarcodeTest.php
@@ -14,10 +14,12 @@ class BarcodeTest extends \PHPUnit_Framework_TestCase
             ->setFormat(Barcode::TYPE_PDF_417)
             ->setMessage('hello')
             ->setMessageEncoding('iso-8859-1')
+            ->setAltText('hello world')
         ;
 
         $this->assertEquals($barcode->getFormat(), Barcode::TYPE_PDF_417);
         $this->assertEquals($barcode->getMessage(), 'hello');
+        $this->assertEquals($barcode->getAltText(), 'hello world');
         $array = $barcode->toArray();
     }
 }
