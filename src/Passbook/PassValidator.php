f927c3a6c8102bffdfecd64dfc2b9dde843bbcf4
Garrett Mears <garrett.mears@coniq.com>
2015-10-21 09:13:37 +0100

    Added PassValidator to try and catch typical errors with passes.

---
+++ src/Passbook/PassValidator.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace Passbook;
+
+use Passbook\Pass\Barcode;
+use Passbook\Pass\Beacon;
+use Passbook\Pass\Image;
+use Passbook\Pass\Location;
+
+/**
+ * Validates the contents of a pass
+ *
+ * This attempts to find errors with a pass that would prevent it from working
+ * properly with Apple Wallet. Passes with errors often just fail to load and
+ * do not provide any feedback as to the error. Additionally, some issues (such
+ * as a pass not having an icon) are not well documented and potentially
+ * difficult to identify. This class aims to help identify and prevent these
+ * issues.
+ */
+class PassValidator
+{
+    private $errors;
+
+    const DESCRIPTION_REQUIRED = 'description is required and cannot be blank.';
+    const FORMAT_VERSION_REQUIRED = 'formatVersion is required and must be 1.';
+    const ORGANIZATION_NAME_REQUIRED = 'organizationName is required and cannot be blank.';
+    const PASS_TYPE_IDENTIFIER_REQUIRED = 'passTypeIdentifier is required and cannot be blank.';
+    const SERIAL_NUMBER_REQUIRED = 'serialNumber is required and cannot be blank.';
+    const TEAM_IDENTIFIER_REQUIRED = 'teamIdentifier is required and cannot be blank.';
+    const ICON_REQUIRED = 'pass must have an icon image.';
+    const BARCODE_FORMAT_INVALID = 'barcode format is invalid.';
+    const BARCODE_MESSAGE_INVALID = 'barcode message is invalid; must be a string';
+    const LOCATION_LATITUDE_REQUIRED = 'location latitude is required';
+    const LOCATION_LONGITUDE_REQUIRED = 'location longitude is required';
+    const LOCATION_LATITUDE_INVALID = 'location latitude is invalid; must be numeric';
+    const LOCATION_LONGITUDE_INVALID = 'location longitude is invalid; must be numeric';
+    const LOCATION_ALTITUDE_INVALID = 'location altitude is invalid; must be numeric';
+    const BEACON_PROXIMITY_UUID_REQUIRED = 'beacon proximity UUID is required';
+    const BEACON_MAJOR_INVALID = 'beacon major is invalid; must be 16-bit unsigned integer';
+    const BEACON_MINOR_INVALID = 'beacon minor is invalid; must be 16-bit unsigned integer';
+
+    public function validate(Pass $pass)
+    {
+        $this->errors = array();
+
+        $this->validateRequiredFields($pass);
+        $this->validateBeaconKeys($pass);
+        $this->validateLocationKeys($pass);
+        $this->validateBarcodeKeys($pass);
+        $this->validateImages($pass);
+
+        return count($this->errors) === 0;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    private function validateRequiredFields(Pass $pass)
+    {
+        if ($this->isBlankOrNull($pass->getDescription())) {
+            $this->addError(self::DESCRIPTION_REQUIRED);
+        }
+
+        if ($pass->getFormatVersion() !== 1) {
+            $this->addError(self::FORMAT_VERSION_REQUIRED);
+        }
+
+        if ($this->isBlankOrNull($pass->getOrganizationName())) {
+            $this->addError(self::ORGANIZATION_NAME_REQUIRED);
+        }
+
+        if ($this->isBlankOrNull($pass->getPassTypeIdentifier())) {
+            $this->addError(self::PASS_TYPE_IDENTIFIER_REQUIRED);
+        }
+
+        if ($this->isBlankOrNull($pass->getSerialNumber())) {
+            $this->addError(self::SERIAL_NUMBER_REQUIRED);
+        }
+
+        if ($this->isBlankOrNull($pass->getTeamIdentifier())) {
+            $this->addError(self::TEAM_IDENTIFIER_REQUIRED);
+        }
+    }
+
+    private function validateBeaconKeys(Pass $pass)
+    {
+        $beacons = $pass->getBeacons();
+
+        foreach ($beacons as $beacon) {
+            $this->validateBeacon($beacon);
+        }
+    }
+
+    private function validateBeacon(Beacon $beacon)
+    {
+        if ($this->isBlankOrNull($beacon->getProximityUUID())) {
+            $this->addError(self::BEACON_PROXIMITY_UUID_REQUIRED);
+        }
+
+        if (null !== $beacon->getMajor()) {
+            if (!is_int($beacon->getMajor()) || $beacon->getMajor() < 0 || $beacon->getMajor() > 65535) {
+                $this->addError(self::BEACON_MAJOR_INVALID);
+            }
+        }
+
+        if (null !== $beacon->getMinor()) {
+            if (!is_int($beacon->getMinor()) || $beacon->getMinor() < 0 || $beacon->getMinor() > 65535) {
+                $this->addError(self::BEACON_MINOR_INVALID);
+            }
+        }
+    }
+
+    private function validateLocationKeys(Pass $pass)
+    {
+        $locations = $pass->getLocations();
+
+        foreach ($locations as $location) {
+            $this->validateLocation($location);
+        }
+    }
+
+    private function validateLocation(Location $location)
+    {
+        if ($this->isBlankOrNull($location->getLatitude())) {
+            $this->addError(self::LOCATION_LATITUDE_REQUIRED);
+        }
+
+        if (!is_numeric($location->getLatitude())) {
+            $this->addError(self::LOCATION_LATITUDE_INVALID);
+        }
+
+        if ($this->isBlankOrNull($location->getLongitude())) {
+            $this->addError(self::LOCATION_LONGITUDE_REQUIRED);
+        }
+
+        if (!is_numeric($location->getLongitude())) {
+            $this->addError(self::LOCATION_LONGITUDE_INVALID);
+        }
+
+        if (!is_numeric($location->getAltitude()) && null !== $location->getAltitude()) {
+            $this->addError(self::LOCATION_ALTITUDE_INVALID);
+        }
+    }
+
+    private function validateBarcodeKeys(Pass $pass)
+    {
+        $validBarcodeFormats = array(Barcode::TYPE_QR, Barcode::TYPE_AZTEC, Barcode::TYPE_PDF_417);
+
+        $barcode = $pass->getBarcode();
+
+        if (!$barcode) {
+            return;
+        }
+
+        if (!in_array($barcode->getFormat(), $validBarcodeFormats)) {
+            $this->addError(self::BARCODE_FORMAT_INVALID);
+        }
+
+        if (!is_string($barcode->getMessage())) {
+            $this->addError(self::BARCODE_MESSAGE_INVALID);
+        }
+    }
+
+    private function validateImages(Pass $pass)
+    {
+        $images = $pass->getImages();
+
+        foreach ($images as $image) {
+            /* @var Image $image */
+            if ($image->getContext() === 'icon') {
+                return;
+            }
+        }
+
+        $this->addError(self::ICON_REQUIRED);
+    }
+
+    private function isBlankOrNull($text)
+    {
+        return '' === $text || null === $text;
+    }
+
+    private function addError($string)
+    {
+        $this->errors[] = $string;
+    }
+
+}
---
+++ tests/Passbook/Tests/PassValidatorTest.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace Passbook\Tests;
+
+use Passbook\Pass;
+use Passbook\Pass\Barcode;
+use Passbook\Pass\Beacon;
+use Passbook\Pass\Image;
+use Passbook\Pass\Location;
+use Passbook\PassValidator;
+use Passbook\Type\Generic;
+
+class PassValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    const SERIAL_NUMBER = '123';
+    const DESCRIPTION = 'description';
+
+    /**
+     * @var Pass
+     */
+    private $pass;
+
+    public function testPassWithoutDescription()
+    {
+        $this->assertFails(new Generic(self::SERIAL_NUMBER, ''), PassValidator::DESCRIPTION_REQUIRED);
+        $this->assertFails(new Generic(self::SERIAL_NUMBER, null), PassValidator::DESCRIPTION_REQUIRED);
+        $this->assertPasses(new Generic(self::SERIAL_NUMBER, '0'), PassValidator::DESCRIPTION_REQUIRED);
+    }
+
+    public function testPassWithoutSerialNumber()
+    {
+        $this->assertFails(new Generic('', self::DESCRIPTION), PassValidator::SERIAL_NUMBER_REQUIRED);
+        $this->assertFails(new Generic(null, self::DESCRIPTION), PassValidator::SERIAL_NUMBER_REQUIRED);
+        $this->assertPasses(new Generic('0', self::DESCRIPTION), PassValidator::SERIAL_NUMBER_REQUIRED);
+    }
+
+    public function testPassFormatVersion()
+    {
+        $this->assertPasses($this->pass, PassValidator::FORMAT_VERSION_REQUIRED);
+        $this->pass->setFormatVersion('');
+        $this->assertFails($this->pass, PassValidator::FORMAT_VERSION_REQUIRED);
+        $this->pass->setFormatVersion(null);
+        $this->assertFails($this->pass, PassValidator::FORMAT_VERSION_REQUIRED);
+        $this->pass->setFormatVersion('0');
+        $this->assertFails($this->pass, PassValidator::FORMAT_VERSION_REQUIRED);
+    }
+
+    public function testPassWithoutOrganizationName()
+    {
+        $this->pass->setOrganizationName('');
+        $this->assertFails($this->pass, PassValidator::ORGANIZATION_NAME_REQUIRED);
+        $this->pass->setOrganizationName(null);
+        $this->assertFails($this->pass, PassValidator::ORGANIZATION_NAME_REQUIRED);
+        $this->pass->setOrganizationName('0');
+        $this->assertPasses($this->pass, PassValidator::ORGANIZATION_NAME_REQUIRED);
+    }
+
+    public function testPassWithoutPassTypeIdentifier()
+    {
+        $this->pass->setPassTypeIdentifier('');
+        $this->assertFails($this->pass, PassValidator::PASS_TYPE_IDENTIFIER_REQUIRED);
+        $this->pass->setPassTypeIdentifier(null);
+        $this->assertFails($this->pass, PassValidator::PASS_TYPE_IDENTIFIER_REQUIRED);
+        $this->pass->setPassTypeIdentifier('0');
+        $this->assertPasses($this->pass, PassValidator::PASS_TYPE_IDENTIFIER_REQUIRED);
+    }
+
+    public function testPassWithoutTeamIdentifier()
+    {
+        $this->pass->setTeamIdentifier('');
+        $this->assertFails($this->pass, PassValidator::TEAM_IDENTIFIER_REQUIRED);
+        $this->pass->setTeamIdentifier(null);
+        $this->assertFails($this->pass, PassValidator::TEAM_IDENTIFIER_REQUIRED);
+        $this->pass->setTeamIdentifier('0');
+        $this->assertPasses($this->pass, PassValidator::TEAM_IDENTIFIER_REQUIRED);
+    }
+
+    public function testPassBarcodeFormat()
+    {
+        // Test first before barcode is added
+        $this->assertPasses($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+        $this->pass->setBarcode(new Barcode(Barcode::TYPE_QR, 'message'));
+        $this->assertPasses($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+
+        $this->pass->setBarcode(new Barcode('', 'message'));
+        $this->assertFails($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+        $this->pass->setBarcode(new Barcode(null, 'message'));
+        $this->assertFails($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+        $this->pass->setBarcode(new Barcode('invalid format', 'message'));
+        $this->assertFails($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+    }
+
+    public function testPassBarcodeMessage()
+    {
+        // Test first before barcode is added
+        $this->assertPasses($this->pass, PassValidator::BARCODE_MESSAGE_INVALID);
+        $this->pass->setBarcode(new Barcode(Barcode::TYPE_QR, 'message'));
+        $this->assertPasses($this->pass, PassValidator::BARCODE_MESSAGE_INVALID);
+        $this->pass->setBarcode(new Barcode(Barcode::TYPE_QR, ''));
+        $this->assertPasses($this->pass, PassValidator::BARCODE_MESSAGE_INVALID);
+
+        $this->pass->setBarcode(new Barcode('', null));
+        $this->assertFails($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+        $this->pass->setBarcode(new Barcode(null, 0));
+        $this->assertFails($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+        $this->pass->setBarcode(new Barcode(null, 123));
+        $this->assertFails($this->pass, PassValidator::BARCODE_FORMAT_INVALID);
+    }
+
+    public function testPassLocation()
+    {
+        $this->assertPasses($this->pass, PassValidator::LOCATION_LONGITUDE_REQUIRED);
+        $this->assertPasses($this->pass, PassValidator::LOCATION_LATITUDE_REQUIRED);
+        $this->assertPasses($this->pass, PassValidator::LOCATION_ALTITUDE_INVALID);
+
+        $location = new Location(0,0);
+        $this->pass->addLocation($location);
+        $this->assertPasses($this->pass, PassValidator::LOCATION_LONGITUDE_REQUIRED);
+        $this->assertPasses($this->pass, PassValidator::LOCATION_LATITUDE_REQUIRED);
+        $this->assertPasses($this->pass, PassValidator::LOCATION_ALTITUDE_INVALID);
+
+        $location->setLatitude(null);
+        $this->assertFails($this->pass, PassValidator::LOCATION_LATITUDE_REQUIRED);
+        $location->setLatitude('');
+        $this->assertFails($this->pass, PassValidator::LOCATION_LATITUDE_REQUIRED);
+        $location->setLatitude('foo');
+        $this->assertFails($this->pass, PassValidator::LOCATION_LATITUDE_INVALID);
+
+        $location->setLatitude(0);
+        $location->setLongitude(null);
+        $this->assertFails($this->pass, PassValidator::LOCATION_LONGITUDE_REQUIRED);
+        $location->setLongitude('');
+        $this->assertFails($this->pass, PassValidator::LOCATION_LONGITUDE_REQUIRED);
+        $location->setLongitude('foo');
+        $this->assertFails($this->pass, PassValidator::LOCATION_LONGITUDE_INVALID);
+
+        $location->setLongitude(0);
+        $location->setAltitude(0);
+        $this->assertPasses($this->pass, PassValidator::LOCATION_ALTITUDE_INVALID);
+
+        $location->setAltitude('');
+        $this->assertFails($this->pass, PassValidator::LOCATION_ALTITUDE_INVALID);
+        $location->setAltitude('foo');
+        $this->assertFails($this->pass, PassValidator::LOCATION_ALTITUDE_INVALID);
+    }
+
+    public function testPassBeacon()
+    {
+        $this->assertPasses($this->pass, PassValidator::BEACON_PROXIMITY_UUID_REQUIRED);
+        $this->assertPasses($this->pass, PassValidator::BEACON_MAJOR_INVALID);
+        $this->assertPasses($this->pass, PassValidator::BEACON_MINOR_INVALID);
+
+        $beacon = new Beacon('2b4fcf51-4eaa-446d-b24e-4d1b437f3840');
+        $this->pass->addBeacon($beacon);
+        $this->assertPasses($this->pass, PassValidator::BEACON_PROXIMITY_UUID_REQUIRED);
+        $this->assertPasses($this->pass, PassValidator::BEACON_MAJOR_INVALID);
+        $this->assertPasses($this->pass, PassValidator::BEACON_MINOR_INVALID);
+
+        $beacon->setMajor('');
+        $this->assertFails($this->pass, PassValidator::BEACON_MAJOR_INVALID);
+        $beacon->setMajor('foo');
+        $this->assertFails($this->pass, PassValidator::BEACON_MAJOR_INVALID);
+        $beacon->setMajor(-1);
+        $this->assertFails($this->pass, PassValidator::BEACON_MAJOR_INVALID);
+        $beacon->setMajor(65536);
+        $this->assertFails($this->pass, PassValidator::BEACON_MAJOR_INVALID);
+
+        $beacon->setMajor(0);
+        $beacon->setMinor('');
+        $this->assertFails($this->pass, PassValidator::BEACON_MINOR_INVALID);
+        $beacon->setMinor('foo');
+        $this->assertFails($this->pass, PassValidator::BEACON_MINOR_INVALID);
+        $beacon->setMinor(-1);
+        $this->assertFails($this->pass, PassValidator::BEACON_MINOR_INVALID);
+        $beacon->setMinor(65536);
+        $this->assertFails($this->pass, PassValidator::BEACON_MINOR_INVALID);
+    }
+
+    public function testPassWithoutIcon()
+    {
+        self::assertArrayNotHasKey('icon', $this->pass->getImages(), 'pass must not have an icon for test to be valid');
+        $this->assertFails($this->pass, PassValidator::ICON_REQUIRED);
+
+        $icon = new Image(__DIR__.'/../../img/icon.png', 'icon');
+        $this->pass->addImage($icon);
+        $this->assertPasses($this->pass, PassValidator::ICON_REQUIRED);
+    }
+
+    private function assertFails($pass, $expectedError)
+    {
+        $validator = new PassValidator();
+        self::assertFalse($validator->validate($pass));
+        self::assertContains($expectedError, $validator->getErrors());
+    }
+
+    private function assertPasses($pass, $unexpectedError)
+    {
+        $validator = new PassValidator();
+        $validator->validate($pass);
+        self::assertNotContains($unexpectedError, $validator->getErrors());
+    }
+
+    protected function setUp()
+    {
+        $this->pass = new Generic(self::SERIAL_NUMBER, self::DESCRIPTION);
+    }
+}
