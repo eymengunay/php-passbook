15e26dffd81054cdcf070e6ce4cd015819f90bdb
Razvan Grigore <razvan.grigore@vampirebyte.ro>
2024-02-15 14:47:20 +0200

    add vanity class aliases for google pass types

---
+++ src/Passbook/Type/Loyalty.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Passbook package.
+ *
+ * (c) Eymen Gunay <eymen@egunay.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Passbook\Type;
+
+/**
+ * Vanity Google alias for Apple StoreCard type
+ *
+ * @author Razvan Grigore <razvan.grigore@vampirebyte.ro>
+ */
+class Loyalty extends StoreCard
+{
+}
